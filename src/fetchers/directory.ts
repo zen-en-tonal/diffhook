3cf86eb9b174f4ce4fc79ca7b80c05002d10e385
Takeru KODAMA <zen.en.tonal@gmail.com>
2023-05-01 06:22:32 +0900

    [Add] directory fetcher

---
+++ src/fetchers/directory.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import { FetchResult, Fetcher } from '../core/fetcher'
+import path from 'path'
+
+export type Directory = {
+    [key in string]: string
+}
+
+export class DirectoryFetcher implements Fetcher<Directory> {
+    
+    constructor(
+        private readonly watching: string
+    ) { }
+
+    get(): Promise<FetchResult<Directory>> {
+        const dirents = fs.readdirSync(this.watching, { withFileTypes: true })
+        const files = dirents.filter(
+            d => d.isFile()
+        ).map(
+            d => ({[d.name]: path.normalize(d.name)})
+        ).reduce(
+            (l, r) => Object.assign(l, r), {}
+        )
+        return Promise.resolve({ ok: true, res: files })
+    }
+
+}
