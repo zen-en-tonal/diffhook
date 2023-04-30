05f6371472995b4da72c0075168c8df15550e3a8
Takeru KODAMA <zen.en.tonal@gmail.com>
2023-04-30 10:14:36 +0900

    [Fix] fix build issue

--- src/generics/webhook.ts
+++ src/generics/webhook.ts
@@ -1,23 +1,24 @@
+import { Content, ContentDelta } from "../core/content"
 import { Error } from "../core/error"
 import { Hook } from "../core/hook"
 import fetch, { Response } from "node-fetch"
 
-type Options<T, Q> = {
-    formatter: (doc: T) => Q
+type Options<T extends Content, Q> = {
+    formatter: (doc: ContentDelta<T>) => Q
 }
 
 const errorMsg = async (resp: Response) => {
     return `${resp.status}: ${await resp.text()}`
 }
 
-export class WebHook<T, Q> implements Hook<T> {
+export class WebHook<T extends Content, Q> implements Hook<T> {
     constructor(
         readonly endpoint: string,
         readonly options: Partial<Options<T, Q>> = {}
     ) { }
 
-    async emit(doc: T): Promise<Error | { ok: true; }> {
-        const payload = this.options.formatter?.(doc) || doc
+    async emit(delta: ContentDelta<T>): Promise<Error | { ok: true; }> {
+        const payload = this.options.formatter?.(delta) || delta
         const resp = await fetch(this.endpoint, { 
             method: 'POST', 
             body: JSON.stringify(payload) 
